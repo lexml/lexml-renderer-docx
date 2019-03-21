a3cf29cc784ff081fef8b0182c85e42621e5774b
joaoraf <joaoraf@gmail.com>
2019-03-21 19:07:32 -0300

    in progress

--- src/test/scala/TestLexmlDocument.scala
+++ src/test/scala/TestLexmlDocument.scala
@@ -8,23 +8,28 @@ import java.io.File
 object TestLexmlDocument extends App {
 
   val src_dir = new File("/tmp/lexml_test")
-  val srcs = src_dir.list().filter(_.endsWith(".xml")).filterNot(_.endsWith(".gen.xml"))
-  def process(i : int) {
+  val srcs = src_dir.listFiles().filter(_.getName.endsWith(".xml")).filterNot(_.getName.endsWith(".gen.xml"))
+  def process(i : Int) {
+    val xml = XML.loadFile(srcs(i))
     
+    val doc = LexmlDocument(xml)
+    
+    val dstFile = new File(srcs(i).getParentFile,
+        srcs(i).getName.replaceAll(".xml$", ".gen.xml"))
+    if(dstFile.getCanonicalPath == srcs(i).getCanonicalPath) {
+      throw new RuntimeException("ops!")
+    }    
+    FileUtils.writeStringToFile(dstFile, 
+        doc.xml().toString(),"utf-8")
+    val pb = new ProcessBuilder() 
+      
   }
-  val threads = (1 to 8).map { n => new Thread("Runner" + n) {
+  val threads = (0 to 7).map { n => new Thread("Runner" + n) {
     var i = n
     while(i < srcs.length) {
       process(i)
       i = i + 8
     }
   }
-  val xml = XML.loadFile("/tmp/t.xml")
-  
-  val doc = LexmlDocument(xml)
-  
-  FileUtils.writeStringToFile(new java.io.File("/tmp/result.txt"), PP.prettyPrint(doc),"utf-8")
-  FileUtils.writeStringToFile(new java.io.File("/tmp/result.xml"), 
-      doc.xml().toString(),"utf-8")
   
 }
